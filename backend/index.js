730020bae3321f6cf06be0e1a104355f6c68d3fe
madhav9757 <madhavsemwal9@gmail.com>
2025-07-27 21:01:00 +0530

    save point 4

--- backend/index.js
+++ backend/index.js
@@ -28,11 +28,8 @@ app.use(cookieParser());
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
-}));
-
-app.options("*", cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 // Test route
